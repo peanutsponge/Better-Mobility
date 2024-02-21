182393006f6ccfa822222909537f4e7f7fbcdf94
Damian <damiankagenaar@gmial.com>
2024-02-21 13:43:14 +0100

    Change to use ClientPlayerEntityMixin instead

--- src/main/java/io/github/peanutsponge/mobility/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/io/github/peanutsponge/mobility/mixin/ClientPlayerEntityMixin.java
@@ -1,45 +1,34 @@
 package io.github.peanutsponge.mobility.mixin;
 
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.*;
 
 import static io.github.peanutsponge.mobility.MobilityConfig.*;
 
 
-@Mixin(value = PlayerEntity.class)
-public abstract class PlayerEntityMixin extends LivingEntity {
+@Mixin(value = ClientPlayerEntity.class)
+public abstract class ClientPlayerEntityMixin extends PlayerEntity {
 
-    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
-        super(entityType, world);
-    }
-//	@Override
-//	private Vec3d adjustMovementForCollisions(Vec3d movement){
-//		boolean onGround = this.isOnGround();
-//		this.setOnGround(true);
-//		Vec3d adjustedMovementForCollisions = super.adjustMovementForCollisions(movement);
-//		this.setOnGround(onGround);
-//		return adjustedMovementForCollisions;
-//	}
 
     @Unique
     private Vec3d velocity;
 
-    @Inject(method = "jump", at = @At("HEAD"))
-    void jumpSpeedGet(CallbackInfo ci) {
+	public ClientPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
+		super(world, pos, yaw, gameProfile);
+	}
+
+	@Override
+	public void jump(){
         this.velocity = this.getVelocity();
         System.out.println("get: " + this.velocity);
         velocity = new Vec3d(velocity.x, (double)this.getJumpVelocity(), velocity.z);
@@ -51,11 +40,7 @@ public abstract class PlayerEntityMixin extends LivingEntity {
             this.velocity = this.velocity.add((double)(-MathHelper.sin(f) * jumpHorizontalVelocityMultiplier),
 				0.0, (double)(MathHelper.cos(f) * jumpHorizontalVelocityMultiplier));
         System.out.println("add: " + this.velocity);
-
-    }
-
-    @Inject(method = "jump", at = @At("TAIL"))
-    void jumpSpeedSet(CallbackInfo ci) {
+		super.jump();
         System.out.println("set: "+ this.velocity);
         this.setVelocity(this.velocity);
         this.velocityDirty = true;
@@ -65,20 +50,31 @@ public abstract class PlayerEntityMixin extends LivingEntity {
         return jumpStrength * this.getJumpVelocityMultiplier() + this.getJumpBoostVelocityModifier();
     }
 
-    @Inject(method = "travel", at = @At("HEAD"))
-    void alwaysSprint(CallbackInfo ci) {
-		this.setSprinting(alwaysSprint||isSprinting());
+	@Shadow private boolean isWalking(){return true;}
+	@Shadow private boolean canSprint(){return true;}
+	@Shadow private boolean canVehicleSprint(Entity entity){return true;}
+
+	 /**
+	  * @author
+	  * @reason
+	  */
+	 @Overwrite
+	 private boolean canStartSprinting() {
+		 return !this.isSprinting() && this.isWalking() && this.canSprint() && !this.isUsingItem() && !this.hasStatusEffect(StatusEffects.BLINDNESS) && (!this.hasVehicle() || this.canVehicleSprint(this.getVehicle())) && !this.isFallFlying();
+	 }
+    @Override
+	public void travel(Vec3d movementInput){
+		if (canStartSprinting())
+        	this.setSprinting(alwaysSprint||isSprinting());
         if (this.isSneaking()) {
             this.setStepHeight(0.6F);
         } else {
             this.setStepHeight(stepHeight);
         }
-        this.abilities.setWalkSpeed(defaultGenericMovementSpeed);
-        this.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).setBaseValue((double)this.abilities.getWalkSpeed());
+        this.getAbilities().setWalkSpeed(defaultGenericMovementSpeed);
+        this.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).setBaseValue((double) this.getAbilities().getWalkSpeed());
+		super.travel(movementInput);
     }
-    @Final
-    @Shadow
-    private PlayerAbilities abilities;
 
 	@Override
 	public boolean hasNoDrag() {
